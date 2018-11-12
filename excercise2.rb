26dfc34a3f5e0398cd3cddbe743210dc0515c32a
Gabriela <gab.jo.cuello@gmail.com>
2018-11-12 13:15:42 -0500

    First commit

---
+++ excercise2.rb
@@ -0,0 +1,13 @@
+#Question 1
+good_tip = 55 * 0.2
+puts good_tip
+
+#Question 2
+answer_2 = "hello" + 2.to_s
+puts answer_2
+
+#Question 3
+puts "The total for question 3 is #{45628 * 7839}"
+
+#Question4
+puts (10 < 20 && 30 < 20) || !(10 == 11)
